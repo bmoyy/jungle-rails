31ddccbfb6aca586304fe029ea32554bfa4aae7e
Bastien Moy <moy.bastien@gmail.com>
2023-07-05 18:02:26 -0700

    adding testing for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+  
+  validates :firstname, :lastname, presence: true
+  validates :password, length: {minimum: 5}, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,154 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it "is valid with first name, last name, email, and passwords" do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+        })
+      expect(@user).to be_valid
+    end
+
+    it "is not valid without a first name" do
+      @user = User.new(
+        {firstname: nil, 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+        })
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: nil, 
+        email: "bmoy@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+        })
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: nil,
+        password: "password",
+        password_confirmation: "password"
+        })
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid when email already exists" do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+        })
+      @user.save
+      
+      @user2 = User.new(
+        {firstname: "Felix", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+        })
+      expect(@user2).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: nil,
+        password_confirmation: "password"
+        })
+      expect(@user).to_not be_valid
+      end
+
+    it 'is not valid without matching passwords' do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: 'password',
+        password_confirmation: "passwordd"
+        })
+      expect(@user).to_not be_valid
+      end
+     
+    it 'is not valid with less than 5 characters' do
+      @user = User.new(
+        {firstname: "Bastien", 
+        lastname: "Moy", 
+        email: "bmoy@gmail.com",
+        password: 'pass',
+        password_confirmation: "pass"
+        })
+      expect(@user).to_not be_valid
+      end 
+    end
+
+    describe 'authenticate_with_credentials' do
+
+      it 'returns user if given correct credentials' do
+        user = User.create(
+          {firstname: "Bastien", 
+          lastname: "Moy", 
+          email: "bmoy@gmail.com",
+          password: 'password',
+          password_confirmation: "password"
+          })
+        result = User.authenticate_with_credentials('bmoy@gmail.com', 'password')
+        expect(result).to eq(user)
+        end
+
+      it 'returns nil if password is incorrect' do
+        @user = User.create(
+          {firstname: "Bastien", 
+          lastname: "Moy", 
+          email: "bmoy@gmail.com",
+          password: 'password',
+          password_confirmation: "password"
+          })
+        result = User.authenticate_with_credentials('bmoy@gmail.com', 'pass')
+        expect(result).to eq(nil)
+        end
+
+      it 'returns user if given additional spaces in email' do
+        @user = User.create(
+          {firstname: "Bastien", 
+          lastname: "Moy", 
+          email: "bmoy@gmail.com",
+          password: 'password',
+          password_confirmation: "password"
+          })
+        result = User.authenticate_with_credentials(' bmoy@gmail.com ', 'password')
+        expect(result).to eq(@user)
+        end
+      
+      it 'returns user if given wrong cases in email' do
+        @user = User.create(
+          {firstname: "Bastien", 
+          lastname: "Moy", 
+          email: "bmoy@gmail.com",
+          password: 'password',
+          password_confirmation: "password"
+          })
+        result = User.authenticate_with_credentials('bMoy@gmail.com', 'password')
+        expect(result).to eq(@user)
+        end
+    end
 end
